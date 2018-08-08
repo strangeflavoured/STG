fb4411c33b160b789b9c1cdd820f565760d8ad0d
strangeflavoured <40430994+strangeflavoured@users.noreply.github.com>
2018-08-08 18:02:36 +0200

    Update setting.py

--- setting.py
+++ setting.py
@@ -105,3 +105,32 @@ def traa(start):
 
 	G.clear()
 	plt.close()
+
+def td(start,delay):
+	Del=delay
+	states=[start]
+	im=[imgs(start)]
+
+	k=0
+	while k<len(states):
+		for j in range(0,len(im[-1])):
+			if im[-1][j] not in states:
+				states.append(im[-1][j])
+		if k>0:
+			im.append(imgs(states[k]))
+		k+=1
+
+	G=nx.DiGraph()
+	G.add_nodes_from(states)
+	for i,j in enumerate(states):
+		for k in range(0,len(im[i])):
+			G.add_edge(j,im[i][k])
+
+	label=labdic(states)
+
+	fig,ax=plt.subplots(1,1)
+	nx.draw_kamada_kawai(G, node_color='w',edgecolors='w',scale=5,node_shape='s',node_size=0,with_labels=True, font_weight='bold',font_color='r',font_size=10,labels=label)
+	plt.show()
+
+	G.clear()
+	plt.close()
